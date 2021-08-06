79087a84173350f0b1082843b4bbe93815159fa2
Evan Pratten <ewpratten@gmail.com>
2021-08-06 13:35:35 -0400

    Inject analytics straight into window

---
+++ js/ga.js
@@ -0,0 +1,2 @@
+document.body.innerHTML += `<script async src="https://www.googletagmanager.com/gtag/js?id=G-65WL7TRZJ6"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());
+gtag('config', 'G-65WL7TRZJ6');</script>`
